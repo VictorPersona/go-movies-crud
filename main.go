2d3f34e42d725fef6dacfc821fa2de4627737614
VictorPersona <gauravtupe00@gmail.com>
2025-04-06 10:07:47 +0530

    define Movie and Director structs and initialize movies slice

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import {"fmt","encoding/json","math/rand","strconv","github.com/gorilla/mux"}
+
+type Movie struct{
+	ID string `json:"id"`
+	ISBN string `json:"isbn"` 
+	DIRECTOR *Director
+}
+
+type Director struct{
+	FirstName string
+	LastName string
+}
+
+var movies []Movie
